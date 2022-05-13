618e25de9f8c8a65913d2d4deec02dfeeb152940
trung-pham-dinh <63339860+trung-pham-dinh@users.noreply.github.com>
2022-05-13 11:22:47 +0700

    rtp header

--- RtpPacket.py
+++ RtpPacket.py
@@ -19,9 +19,36 @@ class RtpPacket:
 		
 		# header[0] = ...
 		# ...
+		for i in range(HEADER_SIZE):
+			header[i] = 0
+
+		byte_mask = 0xFF
+		header[0] |= (version << 6) & byte_mask
+		header[0] |= (padding << 5) & byte_mask
+		header[0] |= (extension << 4) & byte_mask
+		header[0] |= cc & byte_mask
+		# header[1]
+		header[1] |= (marker << 7) & byte_mask
+		header[1] |= pt & byte_mask
+		# header[2] and header[3] for sequence number
+		header[2] = (seqnum & 0xFF00) >> 8 # first 8 bits
+		header[3] = (seqnum & 0xFF) # last 8 bits
+		# header[4:8] for timestamp
+		header[4] = (timestamp >> 24) # first 8 bits
+		header[5] = (timestamp >> 16) & 0xFF # second 8 bits
+		header[6] = (timestamp >> 8) & 0xFF # third 8 bits
+		header[7] = timestamp & 0xFF # last 8 bits
+		# header[9:12] for synchronization source
+		header[8] = (ssrc >> 24) # first 8 bits
+		header[9] = (ssrc >> 16) & 0xFF # second 8bits
+		header[10] = (ssrc >> 8) & 0xFF # third 8 bits
+		header[11] = ssrc & 0xFF # last 8 bits
+
+		self.header = header
 		
 		# Get the payload from the argument
 		# self.payload = ...
+		self.payload = payload
 		
 	def decode(self, byteStream):
 		"""Decode the RTP packet."""
